dc611143de586d983591fdde5cca57dea3650343
wuls <linsheng.wu@beantechs.com>
2022-08-06 16:51:01 +0800

    test

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -13,7 +13,7 @@ const go = () => {
     <div i-carbon-campsite text-4xl inline-block />
     <p>
       <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
+        Vitesse Lite1111
       </a>
     </p>
     <p>
